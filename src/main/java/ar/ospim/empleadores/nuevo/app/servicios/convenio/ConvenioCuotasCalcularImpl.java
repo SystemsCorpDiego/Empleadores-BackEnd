286491e484c61b2ac023f230feab9d84bfe73776
SystemsCorpDiego <bueno.diego.sc@gmail.com>
2025-10-25 11:30:18 -0300

    la 1er cuota del convenio va con Interes en CERO.-

--- src/main/java/ar/ospim/empleadores/nuevo/app/servicios/convenio/ConvenioCuotasCalcularImpl.java
+++ src/main/java/ar/ospim/empleadores/nuevo/app/servicios/convenio/ConvenioCuotasCalcularImpl.java
@@ -153,7 +153,8 @@ public class ConvenioCuotasCalcularImpl implements ConvenioCuotasCalcular {
 		cuota.setNumero(cuotaNro);
 		cuota.setImporte(capitalCuota);
 		cuota.setVencimiento(calculoFechaVtoCuota);
-		cuota.setInteres( convenioInteresService.calcularInteres(cuit, capitalCuota, calculoFechaInicio, calculoFechaVtoCuota) );
+		//cuota.setInteres( convenioInteresService.calcularInteres(cuit, capitalCuota, calculoFechaInicio, calculoFechaVtoCuota) );
+		cuota.setInteres( BigDecimal.ZERO  ); //20251024> la 1er cuota va sin interes.-
 		lst.add(cuota);
 		
 		for ( cuotaNro = 2;  cuotaNro<=cuotas;  cuotaNro++) {
